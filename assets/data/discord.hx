f8c1a5cd4ed36a8b0d3d6edb0915257d08236306
lazy remix <97311877+lazyremixmanordummyplayinggmod@users.noreply.github.com>
2025-02-27 17:08:33 -0600

    sowwy
    ......

--- assets/data/discord.hx
+++ assets/data/discord.hx
@@ -1,50 +1,49 @@
 import funkin.backend.utils.DiscordUtil;
 
 function onGameOver() {
-	DiscordUtil.changePresence('Game Over', PlayState.SONG.meta.displayName + " (" + PlayState.difficulty + ")");
+	DiscordUtil.changePresence('Everything is over...', PlayState.SONG.meta.displayName + "... Just stop...");
 }
 
 function onDiscordPresenceUpdate(e) {
 	var data = e.presence;
 
 	if(data.button1Label == null)
-		data.button1Label = "Codename Engine Discord";
+		data.button1Label = "Come join Us...";
 	if(data.button1Url == null)
-		data.button1Url = "https://discord.gg/2NTCdsQvx4";
+		data.button1Url = "https://discord.gg/tNPqJnZP";
 }
 
 function onPlayStateUpdate() {
 	DiscordUtil.changeSongPresence(
-		PlayState.instance.detailsText,
-		(PlayState.instance.paused ? "Paused - " : "") + PlayState.SONG.meta.displayName + " (" + PlayState.difficulty + ")",
-		PlayState.instance.inst,
-		PlayState.instance.getIconRPC()
+		PlayState.instance.detailsText +,
+		(PlayState.instance.paused ? "Paused... - " : "") + PlayState.SONG.meta.displayName,
+		PlayState.instance.inst +,
+		PlayState.instance.getIconRPC() 
 	);
 }
 
 function onMenuLoaded(name:String) {
-	// Name is either "Main Menu", "Freeplay", "Title Screen", "Options Menu", "Credits Menu", "Beta Warning", "Update Available Screen", "Update Screen"
-	DiscordUtil.changePresenceSince("In the Menus", null);
+	DiscordUtil.changePresenceSince("In the Menus... or are we?", null);
 }
 
 function onEditorTreeLoaded(name:String) {
 	switch(name) {
 		case "Character Editor":
-			DiscordUtil.changePresenceSince("Choosing a Character", null);
+			DiscordUtil.changePresenceSince("Choosing a Character... or erasing them?", null);
 		case "Chart Editor":
-			DiscordUtil.changePresenceSince("Choosing a Chart", null);
+			DiscordUtil.changePresenceSince("Choosing a Chart... The notes won't stop...", null);
 		case "Stage Editor":
-			DiscordUtil.changePresenceSince("Choosing a Stage", null);
+			DiscordUtil.changePresenceSince("Choosing a Stage... the world is collapsing...", null);
 	}
 }
 
 function onEditorLoaded(name:String, editingThing:String) {
 	switch(name) {
 		case "Character Editor":
-			DiscordUtil.changePresenceSince("Editing a Character", editingThing);
+			DiscordUtil.changePresenceSince("Editing a Character... Are you sure you want to?", editingThing);
 		case "Chart Editor":
-			DiscordUtil.changePresenceSince("Editing a Chart", editingThing);
+			DiscordUtil.changePresenceSince("Editing a Chart... Glitching in progress...", editingThing);
 		case "Stage Editor":
-			DiscordUtil.changePresenceSince("Editing a Stage", editingThing);
+			DiscordUtil.changePresenceSince("Editing a Stage... Or breaking it?", editingThing);
 	}
 }
